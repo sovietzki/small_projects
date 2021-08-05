03647a145bb66096abcecdfb4011eb8f023c0c7d
Marcin Sowiecki <msowiecki@gmail.com>
2021-08-05 10:54:13 +0200

    edit of guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -13,25 +13,25 @@ while user_choice != random_number:
     print()
     user_choice = int(input("Please pick your number from 1 to 10: "))
     if user_choice < random_number:      
-        print()
-        print("That's too low, aim higher")
+        
+        print("\nThat's too low, aim higher")
         
     elif user_choice > random_number:
-        print()
-        print("That's too high, but I aprreciate your confidence")
+        
+        print("\nThat's too high, but I aprreciate your confidence")
        
     else:
         
-        print()
-        print("Congratulations, you have guessed it!!!")
+        
+        print("\nCongratulations, you have guessed it!!!")
         print()
         play_again = input("Do you want to play again? [y/n] ")
         if play_again == "y":
             random_number = random.randint(1, 10)
             user_choice = None
         else:
-            print()
-            print("Thank you for playing")
+            
+            print("\nThank you for playing")
             break               
 print()
 
